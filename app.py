7fd43405a4fb77deadd8a761b0496fa8bb3f7021
melloj <jonathon.mellor@ons.gov.uk>
2021-06-16 16:48:59 +0100

    initial setting up spark dummy data and app reworked from Pandas

--- app.py
+++ app.py
@@ -3,32 +3,57 @@ import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+
+from pyspark.sql import SparkSession, DataFrame
 import pandas as pd
 
-df = pd.DataFrame({"Household ID": ["000", "000", "111"], "Name": ["John", "Jane", "Alfred"],"Visit Date": ["", "", ""], "Visit type": ["First", "Follow up", "First"]})
-df = pd.concat([df, df])
-selection_col = "Household ID"
+from generate_dummy_tables import generate_demo_tables
 
-hh_ids = [{key: id for key in ["label", "value"]} for id in df[selection_col].unique()]
+
+
+spark_session = SparkSession.builder.getOrCreate()
+
+generate_demo_tables(spark_session)
+
+ons_hh_id = spark_session.sql("SELECT DISTINCT ons_household_id FROM summary")
+summary_col_names = spark_session.sql("SHOW COLUMNS IN summary")
+
+
+def df_to_list(df: DataFrame):
+    # assume always 1 column
+    return df.toPandas().iloc[:,0].to_list()
+
+def create_options(option_values: list, key_names: list):
+    
+    return [{key: id for key in key_names} for id in option_values]
+
+ons_hh_id_list = df_to_list(ons_hh_id)
+
+hh_id_options = create_options(ons_hh_id_list, ["label", "value"])
+
+summary_col_names_list = df_to_list(summary_col_names)
+
+summary_col_options = create_options(summary_col_names_list, ["name", "id"])
+
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     html.Label('Household ID Selection'),
         dcc.Dropdown(
-            options=hh_ids,
+            options=hh_id_options,
             value=None,
             multi=True,
             id="selection"
         ),
     dash_table.DataTable(
         id='table',
-        columns=[{"name": i, "id": i} for i in df.columns],
+        columns=summary_col_options,
         data=None,
     )
 
 ])
-
+"""
 @app.callback(
     Output(component_id='table', component_property='data'),
     Input(component_id='selection', component_property='value')
@@ -37,7 +62,27 @@ def update_output_div(input_value):
     if input_value is not None:
         return df[df[selection_col].isin(input_value)].to_dict("records")
 
-
+"""
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+
+    """
+    spark_session = SparkSession.builder.getOrCreate()
+
+    generate_demo_tables(spark_session)
+
+    #spark_session.sql("SELECT * FROM summary").show()
+
+    ons_hh_id = spark_session.sql("SELECT DISTINCT ons_household_id FROM summary")
+
+    ons_hh_id_list = ons_hh_id.toPandas()["ons_household_id"].to_list()
+
+    hh_id_options = [{key: id for key in ["label", "value"]} for id in ons_hh_id_list]
+
+    summary_col_names = spark_session.sql("SHOW COLUMNS IN summary").show()
+
+    print(summary_col_names)
+    """
--- generate_dummy_tables.py
+++ generate_dummy_tables.py
@@ -1,23 +1,74 @@
 import os
+from typing import Callable
 import yaml
 from mimesis.schema import Field, Schema
 import pyspark
+from pyspark.sql import SparkSession, DataFrame
+import pandas as pd
 
 _ = Field('en-gb', seed=42)
 
+#$set INCENTIVES_CONFIG_PATH=./config.yaml
 with open(os.environ.get("INCENTIVES_CONFIG_PATH")) as f:
     config = yaml.safe_load(f)
 
-def generate_summary_table():
-    lab_bloods_description = lambda: {
+def define_summary_schema():
+    return lambda: {
         'ons_household_id': _('random.custom_code', mask='COV ############', digit='#'),
         'participant_id': _('random.custom_code', mask='DHR-############', digit='#'),
         'fullname': _('person.full_name'),
         'due_now': _('random.randints', amount=1)[0]
     }
 
-    schema = Schema(schema=lab_bloods_description)
-    return schema
+
+def schema_to_dataframe(session: SparkSession, schema_definition: Callable, num_records: int):
+
+    schema = Schema(schema=schema_definition)
+
+    data = schema.create(iterations=num_records)
+
+    pd_df = pd.DataFrame(data)
+
+    ps_df = session.createDataFrame(pd_df)
+
+    return ps_df
+
+
+def write_to_temp(df: DataFrame, name: str):
+
+    df.createOrReplaceTempView(name)
+
+
+
+
+def generate_temp_table(session: SparkSession, schema_definition: Callable, name: str):
+
+    summary_df = schema_to_dataframe(session, schema_definition, 100)
+
+    write_to_temp(summary_df, name)
+    
+
+
+
+def generate_demo_tables(session: SparkSession):
+
+    generate_temp_table(session, define_summary_schema(), "summary")
+
+    generate_temp_table(session, define_summary_schema(), "summary2")
+
+
 
 if __name__ == "__main__":
-    print(generate_summary_table().create(iterations=4))
+    #print(generate_summary_table().create(iterations=4))
+
+    #$set PYSPARK_PYTHON=D:\Repositories\incentives-env\Scripts\python.exe
+
+    spark_session = SparkSession.builder.getOrCreate()
+
+    summary_schema = define_summary_schema()
+
+    summary_df = schema_to_dataframe(spark_session, summary_schema, 5)
+
+    write_to_temp(summary_df, "summary")
+
+    spark_session.sql("SELECT * FROM summary").show()
